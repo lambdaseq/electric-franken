6668895dc171f83065248e0ddab268c4d268efa7
DrBuchkov <pafitisnick@gmail.com>
2024-05-18 20:27:43 +0300

    Removing unused test dir.

--- test/conjurernix/electric_commons_test.clj
+++
@@ -1,7 +0,0 @@
-(ns conjurernix.electric-franken-test
-  (:require [clojure.test :refer :all]
-            [conjurernix.electric-franken :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
